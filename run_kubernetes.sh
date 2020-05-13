20d6ed9a64abdf8c2cf9cdbb7e8a418cddec97c9
parsaf <parsafd00ni@gmail.com>
2020-05-13 01:17:51 -0700

    Completed first 7 tasks

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=parsafrd/udacity
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment udacity --image=docker.io/${dockerpath}:latest
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/udacity 8000:80
